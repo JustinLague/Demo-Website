add899fa65543af05721c13a210a0bad03d44999
Justin Lague <justin.lague.jl@gmail.com>
2020-12-27 23:01:51 -0500

    main page advance

--- Daria/server.js
+++ Daria/server.js
@@ -3,10 +3,25 @@ var app = express();
 
 app.use(express.static('./'));
 
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/views');
+
 app.get('/', function(req, res) {
     res.render('index.html');
 });
 
+app.get('/review', function(req, res) {
+    res.render('review.html');
+});
+
+app.get('/projet1', function(req, res) {
+    res.render('projet1.html');
+});
+
+app.get('/projet2', function(req, res) {
+    res.render('projet2.html');
+});
+
 const server = app.listen(8080, () => {
     console.log(`Express is running on port ${server.address().port}`);
-});
+}); 
