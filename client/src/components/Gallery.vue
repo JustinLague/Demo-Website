9fbe7534f5e134813a218a545ed3981d800a2059
Justin Lague <justin.lague.jl@gmail.com>
2021-07-30 20:04:37 -0400

    attempt to make it look better

--- client/src/components/Gallery.vue
+++ client/src/components/Gallery.vue
@@ -7,9 +7,12 @@
                 <div class="row">
                     <div class="col-lg-4" v-for="image in galleryImage" :key="image.id">
                         <div class="image">
+                            <b-icon class="icon" scale="4" icon="zoom-in"></b-icon>
                             <enlargeable-image :src=image.thumnailUrl :src_large=image.detailedImageUrl />
-                            <router-link class="more" :to="{name: 'Project', params: { projectId: image.projectId }}">
-                                <p >{{ $t('gallery.more') }}</p>
+                            <router-link :to="{name: 'Project', params: { projectId: image.projectId }}">
+                                <div class="background">
+                                    <p class="more">{{ $t('gallery.more') }}</p>
+                                </div>
                             </router-link>
                         </div>
                     </div>
@@ -59,20 +62,53 @@ a:hover {
 }
 
 .image {
+    position: relative;
     margin-bottom: 30px;
 }
 
-div.full.enlarged {
-    background-image: none !important;
+.more {
+    position: absolute;
+    bottom: 0px;
+    right: 20px;
+    font-weight: bold;
+    color: white;  
+    text-decoration: none;
 }
 
-.more {
-    color: black; 
-    text-decoration: none;
+.background {
+    position: absolute;
+    bottom: 0px;
+    width: 100%;
+    height: 60px;
+    background-color: gray;
+    mix-blend-mode: lighten;
+    opacity: 0;
+    transition: all .2s ease-in-out;
 }
 
 .more:hover {
-    color: #808080;
+    color: #dfdfdf;
     text-decoration: none;
 }
+
+.image:hover .background {
+    opacity: 1;
+}
+
+.icon {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    opacity: 0;
+    color: lightgrey;
+    mix-blend-mode: lighten;
+    transition: all .2s ease-in-out;
+}
+
+.image:hover .icon {
+    opacity: 1;
+}
+
 </style>
